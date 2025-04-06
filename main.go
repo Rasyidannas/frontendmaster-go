c7bbab3acee18811d9118de7d1a3138f751b9200
rasyidannas <rasshit.dsgn@gmail.com>
2025-04-07 05:56:55 +0700

    Part 3 - Loop and Arrays.mp4

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 )
 
 func main() {
+	// CONDITIONAL
 	age := 30
 
 	if age >= 18 {
@@ -28,7 +29,30 @@ func main() {
 			fmt.Println("it's this weekend")
 	}
 
+	// LOOP
+	for i := 0; i < 5; i++ {
+		fmt.Println("this is i", i)
+	}
 
+	counter := 0
+	for counter < 3 {
+		fmt.Println("This is the counter ", counter)
+		counter++
+	}
+
+	// ARRAY
+	numbers := [5]int{10, 20, 30, 40, 5}
+	// numbers := [...]int{10, 20, 30, 40 ,5} // this is same like abovebut use ...
+	numbers[4] = 60 // reassign new value
+
+	fmt.Println("This is the all length of numbers ", len(numbers))
+	
+	matrix := [2][3]int{
+		{1, 2, 3},
+		{4, 5, 6},
+	}
+
+	fmt.Println("This is the matrix: ", matrix)
 }
 
 func add(a int, b int) int {
