4a93527ee0b82b3d6e02a3cffb57ff7d7c460385
rasyidannas <rasshit.dsgn@gmail.com>
2025-05-04 15:44:37 +0700

    Part 4 - Advanced Struct and Pointer

--- main.go
+++ main.go
@@ -5,13 +5,13 @@ import (
 )
 
 //Struct
-type person struct {
-	name string
-	age int
+type Person struct {
+	Name string
+	Age int
 }
 
 func main() {
-	person := person{name: "John", age: 25}
+	person := Person{Name: "John", Age: 25}
 
 	fmt.Printf("This is out person %+v\n", person)
 	
@@ -23,7 +23,44 @@ func main() {
 		name: "alice",
 		id: 123,
 	}
+	
+	type Address struct {
+		Street string
+		City string
+	}
+	
+	// nested struct
+	type Contact struct {
+		Name string
+		Address Address
+		Phone string
+	}
+
+	contact := Contact {
+		Name: "Rasyid",
+		Address: Address{
+			Street: "Jl Jeruck",
+			City: "Sidoarjo",
+		},
+		Phone: "812731766123",
+	}
+
+	fmt.Println("This is contact", contact)
 
 	fmt.Println("This is employee", employee)
+
+	fmt.Println("Name before: ", person.Name)
+	person.modifyPersonName("Rasyid")
+	fmt.Println("Name after: ", person.Name)
+
+	x := 20
+	ptr := &x
+	fmt.Printf("value of x: %d and address of x %p\n", x, ptr)
+	*ptr = 30
+	fmt.Printf("value of x new: %d and address of x %p\n", x, ptr)
 }
 
+func(p *Person) modifyPersonName(name string) {
+	p.Name = name
+	fmt.Println("inside scope: new name", p.Name)
+}
